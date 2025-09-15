9fa31d3c10a13438d49b689215fa1965b72a68d4
joey <joey.sportes@ynov.com>
2025-09-15 16:12:07 +0200

    jsgn

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module red
 
 go 1.25.0
+
+require github.com/01-edu/z01 v0.2.0
---
+++ pagedegarde.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println(`                                                         
+                                                               
+RRRRRRRRRRRRRRRRR   EEEEEEEEEEEEEEEEEEEEEEDDDDDDDDDDDDD        
+R::::::::::::::::R  E::::::::::::::::::::ED::::::::::::DDD     
+R::::::RRRRRR:::::R E::::::::::::::::::::ED:::::::::::::::DD   
+RR:::::R     R:::::REE::::::EEEEEEEEE::::EDDD:::::DDDDD:::::D  
+  R::::R     R:::::R  E:::::E       EEEEEE  D:::::D    D:::::D 
+  R::::R     R:::::R  E:::::E               D:::::D     D:::::D
+  R::::RRRRRR:::::R   E::::::EEEEEEEEEE     D:::::D     D:::::D
+  R:::::::::::::RR    E:::::::::::::::E     D:::::D     D:::::D
+  R::::RRRRRR:::::R   E:::::::::::::::E     D:::::D     D:::::D
+  R::::R     R:::::R  E::::::EEEEEEEEEE     D:::::D     D:::::D
+  R::::R     R:::::R  E:::::E               D:::::D     D:::::D
+  R::::R     R:::::R  E:::::E       EEEEEE  D:::::D    D:::::D 
+RR:::::R     R:::::REE::::::EEEEEEEE:::::EDDD:::::DDDDD:::::D  
+R::::::R     R:::::RE::::::::::::::::::::ED:::::::::::::::DD   
+R::::::R     R:::::RE::::::::::::::::::::ED::::::::::::DDD     
+RRRRRRRR     RRRRRRREEEEEEEEEEEEEEEEEEEEEEDDDDDDDDDDDDD
+`)                                                                 
+}
