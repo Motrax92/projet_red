43f11abf7ca689401bfc515d93ae70bc023de704
Mohamed <mohamedsayd.traore@ynov.com>
2025-09-16 10:31:12 +0200

    musique
    Refactor music playback to use an infinite loop and manual closure.

--- musique.go
+++ musique.go
@@ -11,27 +11,33 @@ import (
 )
 
 func Musique() {
+	// Ouvrir le fichier MP3
 	f, err := os.Open("musique.mp3")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
+	// Décoder le MP3
 	streamer, format, err := mp3.Decode(f)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer streamer.Close()
+	// ⚠️ ne pas defer streamer.Close() ici, sinon ça coupe la boucle
+	// on ferme manuellement plus tard si besoin
 
+	// Initialiser le speaker
 	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	done := make(chan bool)
-	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-		done <- true
-	})))
+	// Créer une boucle infinie
+	loop := beep.Loop(-1, streamer)
 
-	<-done 
+	// Jouer la musique en boucle
+	speaker.Play(loop)
+
+	// Bloquer pour que le programme tourne en continu
+	select {} // boucle infinie
 }
