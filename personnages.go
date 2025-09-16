2ce3e309a1e67d2bcbb8795178aedd7cb5c828ef
romain <romain.tholle@ynov.com>
2025-09-16 14:18:15 +0200

    personnages

--- personnages.go
+++ personnages.go
@@ -1,1 +1,35 @@
 package red
+
+import "fmt"
+
+type Personnage struct {
+	Nom   string
+	Vie   int
+	Force int
+}
+
+func (p *Personnage) EstVivant() bool {
+	return p.Vie > 0
+}
+
+func (p *Personnage) SubirDegats(degats int) {
+	p.Vie -= degats
+	if p.Vie < 0 {
+		p.Vie = 0
+	}
+}
+
+func (p *Personnage) Attaquer(cible *Personnage) {
+	fmt.Printf("%s attaque %s et inflige %d dégâts !\n", p.Nom, cible.Nom, p.Force)
+	cible.SubirDegats(p.Force)
+}
+
+type Joueur struct {
+	Personnage
+	Niveau int
+}
+
+type Ennemi struct {
+	Personnage
+	Type string
+}
