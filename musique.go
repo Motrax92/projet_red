64da75fb1a8a5018b3ef9457f749c5533c677804
Mohamed <mohamedsayd.traore@ynov.com>
2025-09-16 09:32:00 +0200

    musique

--- musique.go
+++ musique.go
@@ -1,2 +1,41 @@
 package red
 
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+)
+
+func musique() {
+	// Ouvrir le fichier MP3
+	f, err := os.Open("musique.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// Décoder le MP3
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+
+	// Initialiser le speaker
+	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Jouer le son
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done // attendre la fin de la musique
+}
